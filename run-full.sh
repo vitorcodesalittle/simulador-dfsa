d077380e6549d3d38d74ed87d78a38baf405315c
vitorcodesalittle <vlma@cin.ufpe.br>
2021-12-15 14:15:20 -0300

    better run full

--- run-full.sh
+++ run-full.sh
@@ -1,10 +1,12 @@
-mkdir -q build
+mkdir -p build
 cmake -S ./simulador -B ./build
 cd build; make; cd ..;
 cp build/simulador ./sim
-sim --out plots/out 
+echo 'Running simulator'
+./sim --out plots/out 
 cd plots/
-mkdir images
+mkdir -p images
+echo 'Generating scripts'
 python main.py out.csv images/
 cd ..
 echo 'Images saved at plots/images'
