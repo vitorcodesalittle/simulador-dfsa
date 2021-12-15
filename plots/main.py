93985bc60e7cf030625595da6c564cc1c4b13449
vitorcodesalittle <vlma@cin.ufpe.br>
2021-12-15 13:11:10 -0300

    fix plots

--- plots/main.py
+++ plots/main.py
@@ -30,11 +30,17 @@ class MarkerHandler(HandlerBase):
         return [plt.Line2D([width/2], [height/2.],ls="",
                            marker=tup[1],color=tup[0], transform=trans)]
 
+def get_estimator_rows(estimator):
+    return df['estimador'] == estimator
 def plot_total_slots(df):
-    plt.plot(df['ntags'], df[df['estimador'] == 'lb']['total_slots'], marker='+')
-    #     plt.plot(df['ntags'], df[df['estimador'] == 'shoute']['total_slots'], marker='o')
-    #     plt.plot(df['ntags'], df[df['estimador'] == 'eom-lee']['total_slots'], marker='s')
-    #     plt.plot(df['ntags'], df[df['estimador'] == 'iv2']['total_slots'], marker='^')
+    lb_estimators = get_estimator_rows('lb')
+    shoute_estimators = get_estimator_rows('shoute')
+    eomlee_estimators = get_estimator_rows('eom-lee')
+    iv2_estimators = get_estimator_rows('iv2')
+    plt.plot(df[lb_estimators]['ntags'], df[lb_estimators]['total_slots'], marker='+', color='b')
+    plt.plot(df[shoute_estimators]['ntags'], df[shoute_estimators]['total_slots'], marker='o', color='g')
+    plt.plot(df[eomlee_estimators]['ntags'], df[eomlee_estimators]['total_slots'], marker='s', color='y')
+    plt.plot(df[iv2_estimators]['ntags'], df[iv2_estimators]['total_slots'], marker='^', color='r')
     ax.legend(list(zip(list_color,list_mak)), list_lab, handler_map={tuple:MarkerHandler()})
     ax.set_ylim([0,3500])
     ax.set_xlabel("Número de etiquetas")
@@ -42,10 +48,14 @@ def plot_total_slots(df):
     save_and_clf(os.path.join(plots_path, "total_slots.png"))
 
 def plot_total_collisions(df):
-    plt.plot(df['ntags'], df[df['estimador'] == 'lb']['total_collisions_slots'], marker='+')
-    #     plt.plot(df['ntags'], df[df['estimador'] == 'shoute']['total_collisions_slots'], marker='o')
-    #     plt.plot(df['ntags'], df[df['estimador'] == 'eom-lee']['total_collisions_slots'], marker='s')
-    #     plt.plot(df['ntags'], df[df['estimador'] == 'iv2']['total_collisions_slots'], marker='^')
+    lb_estimators = get_estimator_rows('lb')
+    shoute_estimators = get_estimator_rows('shoute')
+    eomlee_estimators = get_estimator_rows('eom-lee')
+    iv2_estimators = get_estimator_rows('iv2')
+    plt.plot(df[lb_estimators]['ntags'], df[lb_estimators]['total_collisions_slots'], marker='+', color='b')
+    plt.plot(df[shoute_estimators]['ntags'], df[shoute_estimators]['total_collisions_slots'], marker='o', color='g')
+    plt.plot(df[eomlee_estimators]['ntags'], df[eomlee_estimators]['total_collisions_slots'], marker='s', color='y')
+    plt.plot(df[iv2_estimators]['ntags'], df[iv2_estimators]['total_collisions_slots'], marker='^', color='r')
     ax = plt.gca()
     ax.legend(list(zip(list_color,list_mak)), list_lab, handler_map={tuple:MarkerHandler()})
     ax.set_ylim([0,1800])
@@ -54,10 +64,14 @@ def plot_total_collisions(df):
     save_and_clf(os.path.join(plots_path, "total_collisions.png"))
 
 def plot_total_empty_slots(df):
-    plt.plot(df['ntags'], df[df['estimador'] == 'lb']['total_empty_slots'], marker='+')
-    #     plt.plot(df['ntags'], df[df['estimador'] == 'shoute']['total_empty_slots'], marker='o')
-    #     plt.plot(df['ntags'], df[df['estimador'] == 'eom-lee']['total_empty_slots'], marker='s')
-    #     plt.plot(df['ntags'], df[df['estimador'] == 'iv2']['total_empty_slots'], marker='^')
+    lb_estimators = get_estimator_rows('lb')
+    shoute_estimators = get_estimator_rows('shoute')
+    eomlee_estimators = get_estimator_rows('eom-lee')
+    iv2_estimators = get_estimator_rows('iv2')
+    plt.plot(df[lb_estimators]['ntags'], df[lb_estimators]['total_empty_slots'], marker='+', color='b')
+    plt.plot(df[shoute_estimators]['ntags'], df[shoute_estimators]['total_empty_slots'], marker='o', color='g')
+    plt.plot(df[eomlee_estimators]['ntags'], df[eomlee_estimators]['total_empty_slots'], marker='s', color='y')
+    plt.plot(df[iv2_estimators]['ntags'], df[iv2_estimators]['total_empty_slots'], marker='^', color='r')
     ax = plt.gca()
     ax.legend(list(zip(list_color,list_mak)), list_lab, handler_map={tuple:MarkerHandler()})
     ax.set_ylim([0,1100])
@@ -66,13 +80,16 @@ def plot_total_empty_slots(df):
     save_and_clf(os.path.join(plots_path, "total_empty_slots.png"))
 
 def plot_total_time(df):
-    plt.plot(df['ntags'], df[df['estimador'] == 'lb']['total_empty_slots'], marker='+')
-    #     plt.plot(df['ntags'], df[df['estimador'] == 'shoute']['total_empty_slots'], marker='o')
-    #     plt.plot(df['ntags'], df[df['estimador'] == 'eom-lee']['total_empty_slots'], marker='s')
-    #     plt.plot(df['ntags'], df[df['estimador'] == 'iv2']['total_empty_slots'], marker='^')
+    lb_estimators = get_estimator_rows('lb')
+    shoute_estimators = get_estimator_rows('shoute')
+    eomlee_estimators = get_estimator_rows('eom-lee')
+    iv2_estimators = get_estimator_rows('iv2')
+    plt.plot(df[lb_estimators]['ntags'], df[lb_estimators]['time'], marker='+', color='b')
+    plt.plot(df[shoute_estimators]['ntags'], df[shoute_estimators]['total_empty_slots'], marker='o', color='g')
+    plt.plot(df[eomlee_estimators]['ntags'], df[eomlee_estimators]['total_empty_slots'], marker='s', color='y')
+    plt.plot(df[iv2_estimators]['ntags'], df[iv2_estimators]['total_empty_slots'], marker='^', color='r')
     ax = plt.gca()
     ax.legend(list(zip(list_color,list_mak)), list_lab, handler_map={tuple:MarkerHandler()})
-    ax.set_ylim([0,10])
     ax.set_xlabel("Número de etiquetas")
     ax.set_ylabel("Tempo para identificação")
     save_and_clf(os.path.join(plots_path, "total_time.png"))
