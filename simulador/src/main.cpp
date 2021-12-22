cc32c132238534438979d92efb3f4fa7b5dd8d2a
vitor <vlma@cin.ufpe.br>
2021-12-22 11:28:10 -0300

    using many seeds

--- simulador/src/main.cpp
+++ simulador/src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include<chrono>
 #include <map>
 #include <fstream>
 #include "config.h"
@@ -38,20 +38,41 @@ void save_csv(const string &filepath, const map<pair<int, string>, Result> &resu
     f.close();
 }
 
+// from https://stackoverflow.com/questions/31255486/c-how-do-i-convert-a-stdchronotime-point-to-long-and-back
+long get_seed() {
+    auto now =chrono::high_resolution_clock::now();
+    chrono::time_point_cast<chrono::nanoseconds>(now);
+    auto epoch = now.time_since_epoch();
+    auto value = chrono::duration_cast<chrono::nanoseconds>(epoch);
+    long duration = value.count();
+    return duration;
+}
+
+
+
 int main(int argc, char **argv) {
     ExperimentConfig c = ExperimentConfig::parse(argc, argv);
+        // srand(time(NULL));
     cout << c.to_string() << endl;
     map<pair<int, string>, Result> results;
-    int t = c.estimators.size() * (c.max_tags-c.initial_tags) / c.tag_increment * c.repetitions, i = 0;
+    int t = c.estimators.size() * c.repetitions * (c.max_tags-c.initial_tags+ c.tag_increment - 1) / c.tag_increment, i = 0;
     for (const string& estimatorName : c.estimators) {
         Estimator *e;
         e = create_estimator(estimatorName);
         if (e == nullptr) continue;
         for (int ntags = c.initial_tags;  ntags <= c.max_tags; ntags += c.tag_increment) {
             Result total{};
+            long last = get_seed();
             for (int r = 0; r < c.repetitions; r++) {
                 Simulator s = Simulator(c.initial_frame, *e, !c.no_power_of_2);
+                srand(last);
                 Result result = s.run(ntags);
+                auto new_seed = get_seed();
+                if (new_seed == last) {
+                    cerr << "SAME SEED AGAIN\n";
+                    return 1;
+                } else last = new_seed;
+
                 total += result;
                 if (i % 100 == 0){
                     cout.flush();
