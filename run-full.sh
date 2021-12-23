86ecf4b0b03aa5c731bbc7574f7fed0b586652fe
vitor <vlma@cin.ufpe.br>
2021-12-22 23:25:12 -0300

    64 sized frame plots

--- run-full.sh
+++ run-full.sh
@@ -3,13 +3,13 @@ cmake -S ./simulador -B ./build
 cd build; make; cd ..;
 cp build/simulador ./sim
 echo 'Running simulator'
-./sim --out plots/out --repetitions 2000 --initial-frame 128 --estimators lb,shoute,eom-lee,iv2
+./sim --out plots/out --repetitions 2000 --initial-frame 64 --estimators lb,shoute,eom-lee,iv2
 cd plots/
 mkdir -p images
 echo 'Generating plots'
 python main.py out.csv images/
 cd ..
-./sim --out plots/out-po2-frames --repetitions 2000  --no-po2 --initial-frame 128 --estimators lb,shoute,eom-lee,iv2
+./sim --out plots/out-po2-frames --repetitions 2000  --no-po2 --initial-frame 64 --estimators lb,shoute,eom-lee,iv2
 cd plots/
 mkdir -p images-po2
 python main.py out-po2-frames.csv images-po2/
